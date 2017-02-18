2d9b2083c35e39f38b06191994e2e8495bcd9d90
Karin Jonsson <jonsson.karin.ae@gmail.com>
2017-02-18 14:15:12 +0100

    14

--- Site/scripts/react13.js
+++ Site/scripts/react13.js
@@ -1,4 +1,4 @@
-// Uppgift 1
+// Uppgift 1.1
 const element = (
   <h1>
     React är knasigt!
@@ -10,14 +10,14 @@ ReactDOM.render(
   document.getElementById('first-div')
 );
 
-// Uppgift 2
+// Uppgift 1.2
 const added = (<p>Talet är: {333 + 777}</p>);
 
 ReactDOM.render(
     added, 
     document.getElementById("second-div"));
 
-// Uppgift 3
+// Uppgift 1.3
 const multiple = (<div><h2>Flera element</h2>
                  <p>Här står det massa text, eller ja, inte så mycket egentligen.</p>
                  <ul>
@@ -31,7 +31,7 @@ ReactDOM.render(
 
 
 
-//Uppgift 4
+//Uppgift 1.4
 
 class MyElement extends React.Component {
   render() {
@@ -46,8 +46,8 @@ ReactDOM.render(
     <MyElement specialheader="This header text is a prop!"/>,
 document.getElementById('fourth-div'));
       
-      
-//Test
+/*
+//lite extra test till på uppgift 4
  class MyElement2 extends React.Component {
   render() {
       
@@ -60,3 +60,86 @@ document.getElementById('fourth-div'));
 ReactDOM.render(
 <MyElement2 something="Render in different div" sometext="Nu kan väl bara den här idiotiska texten visas, eller?!"/>,
 document.getElementById('fifth-div'));
+      */
+      
+      
+//Uppgift 2.1
+      
+      
+let someList = ["hej", "hallå", "hejsan", "tja"];      
+class MyList extends React.Component {
+
+    render() {
+        
+        const newList = this.props.list.map(item => (<li key={item}>{item}</li>))
+        return (<ul>{newList}</ul>);
+    }
+}
+                
+ReactDOM.render(<MyList list={someList}/>,
+                document.getElementById("fifth-div"));                
+
+//Uppgift 2.2-2.3
+class Book extends React.Component{
+        constructor(props){
+        super(props);
+        this.state = {status: "okänd"}
+        this._changeStatusState = this._changeStatusState.bind(this);
+    }
+    
+    _changeStatusState(){
+        
+        let statusclass;
+        
+        if(this.state.status =="book-unread"){
+            statusclass = "book-reading";
+        }
+        else if(this.state.status =="book-reading"){
+            statusclass = "book-finished";
+        }
+        else{
+            statusclass = "book-unread";
+        }
+        
+        this.setState({status: statusclass});
+    }
+    render(){
+        
+        let readstatus;
+        
+        if(this.state.status =="book-unread"){
+            readstatus = "oläst";
+        }
+        else if(this.state.status =="book-finished"){
+            readstatus = "läst";
+        }
+        else if(this.state.status =="book-reading"){
+            readstatus = "läsning pågår"
+        }
+        else{
+            readstatus = "okänd";
+        }
+        
+        return (<p onClick={this._changeStatusState} className={this.state.status}>{this.props.author}: {this.props.title}, status: {readstatus}</p>);
+    }
+}
+
+ReactDOM.render(
+      (<div>
+          <Book author ="Någon" title ="Något"/>
+          <Book author ="Någon annan" title ="Något annat"/>
+          <Book author ="Elof" title ="Elof är bäst"/>
+        </div>),
+        document.getElementById("sixth-div"));
+        
+
+        
+        
+        /*
+       Flytta över status till state och lägg till en onClick funktion som byter status när användaren klickar på bok-elementet.
+
+Ändra i render-funktionen så att den skriver ut en textsträng som förklarar vad boken har för status, till exempel "oläst" eller "läst ut".
+
+
+        
+        */
---
+++ Site/scripts/react14.js
@@ -0,0 +1,31 @@
+let wordlist = ["apa", "lapa", "gapa", "rapa", "skapa"];
+let translations = [{word: "katt", translation: "cat" }, 
+                    {word: "hund", translation: "dog" }, 
+                    {word: "marsvin", translation: "guineapig" }, 
+                    {word: "guldfisk", translation: "goldfish" }];
+
+console.log(translations);
+
+class MyList extends React.Component {
+    render(){
+        const renderlist = this.props.list.map(x => <li key={x}>{x}</li>);
+        return (<div><h3>Lista med ord</h3><ul>{renderlist}</ul></div>);
+    }
+}
+                                               
+ReactDOM.render(
+        <MyList list={wordlist}/>, 
+        document.getElementById("react-div")
+        );                                            
+        
+
+class TranslationList extends React.Component {
+    render() {
+        const itemlist = this.props.list.map(obj => <li key={obj.word}>{obj.word} = {obj.translation}</li>);
+        return (<div><h3>Lista med objekt</h3><ul>{itemlist}</ul></div>);
+    }
+}
+                                             
+ReactDOM.render(
+            <TranslationList list={translations}/>, 
+            document.getElementById("react-div2"));
