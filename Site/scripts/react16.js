11c05b8552f6841226286ee5083f410ccc723ba1
Karin Jonsson <jonsson.karin.ae@gmail.com>
2017-02-21 20:19:43 +0100

    css

--- Site/scripts/react16.js
+++ Site/scripts/react16.js
@@ -7,8 +7,9 @@ class ObjectDisplay extends React.Component{
     
     render(){
         return(<div>
-               <h2>hej</h2>
+               <h2>Table of Countries</h2>
                <ObjectTable list={this.state.data}/>
+               <p>Number of countries: {this.state.itemcount}</p>
               
                </div>);
     }
@@ -42,14 +43,14 @@ class ObjectDisplay extends React.Component{
 class ObjectTable extends React.Component{
     render(){
         
-        const tablerows = this.props.list.map(x => <tr key={x.name}><td>{x.name}</td><td>{x.continent}</td><td>{x.population}</td></tr>);
+        const tablerows = this.props.list.map(x => <tr key={x.name}><td>{x.name}</td><td>{x.continent}</td><td className="number-column">{x.population}</td></tr>);
         
-        return(<table>
+        return(<table className="fancy-table">
                 <thead>
                     <tr>
-                        <th>Land</th>
-                        <th>Kontinent</th>
-                        <th>Befolkning</th>
+                        <th>Country</th>
+                        <th>Continent</th>
+                        <th>Population</th>
                     </tr>
                </thead>
                <tbody>
@@ -60,9 +61,9 @@ class ObjectTable extends React.Component{
 }
     
 /*
+Användaren ska kunna klicka på ett element i listan för att markera det. Då ska det dyka upp ett sätt som användaren kan ta bort elementet.
+(Tips: använd conditional rendering.)
 
-Din uppgift är att rendera en React-komponent som gör ett AJAX request och hämta en lista med JSON objekt. Börja med att välja ut ett lämpligt API. (Det är ok att använda landsinformationen på http://forverkliga.se/JavaScript/api/simple.php?world Observera att det ska vara en lista med objekt.)
-AJAX ska göras när komponenten renderats första gången. Innan något svar kommit ska det stå ett lämpligt meddelande på sidan.
 
 */
 
