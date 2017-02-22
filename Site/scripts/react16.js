ff0fc68a0f20617b862616b6e3ead7f2041bc97a
Karin Jonsson <jonsson.karin.ae@gmail.com>
2017-02-22 13:56:25 +0100

    16 m remove row

--- Site/scripts/react16.js
+++ Site/scripts/react16.js
@@ -2,13 +2,14 @@ class ObjectDisplay extends React.Component{
     constructor(props){
         super(props);
         this.fetchData = this.fetchData.bind(this);
+        this.removeItem = this.removeItem.bind(this);
         this.state = { data: [], itemcount: 0};
     }
     
     render(){
         return(<div>
                <h2>Table of Countries</h2>
-               <ObjectTable list={this.state.data}/>
+               <ObjectTable list={this.state.data} removeEvent = {this.removeItem}/>
                <p>Number of countries: {this.state.itemcount}</p>
               
                </div>);
@@ -32,18 +33,34 @@ class ObjectDisplay extends React.Component{
             console.log("I've got json and I'm not afraid to use it");
             
             _this.setState({data: json, itemcount: json.length});
-            console.log("ja!"); 
+            console.log("data saved in state"); 
         });
     
             
    }
+
+    removeItem(event){
+        console.log("remove item table button clicked");
+        event.stopPropagation();
+        //let item = event.currentTarget;
+        let row = event.target.parentElement.parentElement;
+        //console.log(item);
+        console.log(row);
+
+        let list = this.state.data;
+        let newList = list.filter(x => x.name !== row.id);
+        
+        this.setState({data: newList, itemcount: newList.length});
+    }
 }
     
     
 class ObjectTable extends React.Component{
+    
+    
     render(){
         
-        const tablerows = this.props.list.map(x => <tr key={x.name}><td>{x.name}</td><td>{x.continent}</td><td className="number-column">{x.population}</td></tr>);
+        const rows = this.props.list.map(x => <ObjectTableRow key={x.name} item={x} clickEvent={this.props.removeEvent}/>);
         
         return(<table className="fancy-table">
                 <thead>
@@ -54,17 +71,50 @@ class ObjectTable extends React.Component{
                     </tr>
                </thead>
                <tbody>
-                    {tablerows}
-               </tbody>
+               {rows}
+                
+             </tbody>
                </table>);
     }
 }
+
+
+                   
+                   
+class ObjectTableRow extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {clicked: false};
+        this.setClickedState = this.setClickedState.bind(this);
+            }
+        
+setClickedState(){
+     
+    if(this.state.clicked === false){
+        this.setState({class: "test", clicked: true});
+    }
+    else{
+       this.setState({class: "", clicked: false});
+    }   
+}
+        
+render(){
     
-/*
-Användaren ska kunna klicka på ett element i listan för att markera det. Då ska det dyka upp ett sätt som användaren kan ta bort elementet.
-(Tips: använd conditional rendering.)
+    const item = this.props.item;
+    let row;
+    
+    if(this.state.clicked === false){
+        row =(<tr key={item.name} id={item.name}  onClick={this.setClickedState}><td>{item.name}</td><td>{item.continent}</td><td className="number-column">{item.population}</td></tr>);
+    }
+    else {
+        row = (<tr key={item.name} id={item.name} onClick={this.setClickedState}><td>{item.name}</td><td>{item.continent}</td><td className="number-column">{item.population}</td><td><button onClick={this.props.clickEvent}>Remove row</button></td></tr>);
+    }
+    /*
+    return(<tr key={item.name} id={item.name} className={this.state.class} onClick={this.setClickedState}><td>{item.name}</td><td>{item.continent}</td><td className="number-column">{item.population}</td></tr>); */
+    
+    return row;
+    }
+}                
 
-
-*/
 
 ReactDOM.render(<ObjectDisplay/>, document.getElementById("react-div"));
